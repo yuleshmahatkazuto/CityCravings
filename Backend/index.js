639a1c4cbc822a320da83a5797ea01172f9745ed
Yulesh Mahat <yuleshkazuto@gmail.com>
2025-04-23 15:31:30 +1000

    update to front-end back-end integration

--- Backend/index.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import bodyParser from "body-parser";
-
-const app = express();
-const port = 4000;
-const SUPABASE_DB_CONNECTION_STRING =
-  "postgresql://postgres.tszxuchduyrgzxqyxxps:Yulrubis%4058@aws-0-ap-southeast-2.pooler.supabase.com:5432/postgres";
